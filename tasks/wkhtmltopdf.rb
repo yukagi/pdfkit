e94dc8bc2244ad3684ce93a869d8b6d193e77318
Ian Cordasco <graffatcolmingov@gmail.com>
2013-08-15 21:08:05 -0500

    Re-add the gem dependency
    Actually print the version info

--- tasks/wkhtmltopdf.rb
+++ tasks/wkhtmltopdf.rb
@@ -3,7 +3,7 @@ require 'rake'
 namespace :wkhtmltopdf do
   desc "Print the wkhtmltopdf version"
   task :version do
-    wk = `which wkhtmltopdf`
+    wk = `which wkhtmltopdf`.chomp
     puts wk
     puts `#{wk} --version`
   end
